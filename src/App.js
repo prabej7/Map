e6df926283115b05037c92c10a788fe861ced440
Prabej Hussain <contact.prabej@gmail.com>
2024-03-04 08:34:41 +0545

    Add search an api call

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
 import './App.css';
-import { MapContainer, TileLayer, useMap,Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 function App() {
-
+  const [city, setCity] = useState(null);
+  const [err, setErr] = useState(false);
   const [location, setLocation] = useState({
-    lat:27.0449,
-    lon:84.8672
+    lat: 27.0449,
+    lon: 84.8672
   });
 
-  useEffect(()=>{
-    if('geolocation' in navigator){
-      navigator.geolocation.watchPosition((location)=>{
-        setLocation(preValue=>{
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.watchPosition((location) => {
+        setLocation(preValue => {
           return {
             lat: location.coords.latitude,
             lon: location.coords.longitude
           }
         })
-      },(err)=>{
+      }, (err) => {
         console.log(err);
       })
     }
-  },[]);
+  }, []);
 
-  const stations = [
-    {
-      name: 'Birgunj',
-      lat:27.024131364592986,
-      lon:84.88172840787506
-    },
-    {
-      name: 'Simra',
-      lat:27.156149550010298, 
-      lon:84.97527096441785,
-    }
-  ];
 
   function MyMapComponent() {
     const map = useMap();
@@ -44,31 +34,33 @@ function App() {
     return null;
   }
 
-  function handleChange(e){
-    stations.forEach((station)=>{
-      if(station.name===e.target.value){
-        setLocation(preValue=>{
-          return {
-            lat: station.lat,
-            lon: station.lon
-          }
-        });
-      }
-    });
+  function handleChange(e) {
+    setCity(e.target.value);
   };
 
+  function handleOff(){
+    setErr(false);
+  }
 
+  async function handleClick() {
+    const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.REACT_APP_API_KEY}`);
+    if(response.data[0]!==undefined){
+      const {lat,lon} = response.data[0];
+      if(lat!==undefined||lon!==undefined){
+        setLocation(preValue=>({
+          lat:lat,
+          lon: lon
+        }));
+      }
+    }else{
+      setErr(true);
+    }
 
-  // const markers = [
-  //   {
-  //     geocode: [location.lat,location.lon],
-  //     popUp: 'Brigunj'
-  //   }
-  // ];
+  }
 
   const customIcon = new Icon({
-     iconUrl : require('./marker.png'),
-     iconSize:[38,38]
+    iconUrl: require('./marker.png'),
+    iconSize: [38, 38]
   });
   return (
     <div className="App">
@@ -76,17 +68,28 @@ function App() {
         <div className="camera"></div>
         <div className="display">
           <div className="artboard artboard-demo phone-1">
-          <select className='dropdown-content' onChange={handleChange}>
-            <option>Birgunj</option>
-            <option>Simra</option>
-          </select>
+
+            <div className='form'>
+              <input
+                type='text'
+                placeholder='City name'
+                onChange={handleChange}
+              />
+              <button onClick={handleClick}>Search</button>
+            </div>
+
+            <div className='error-pop' style={{display:err ? 'flex' : 'none'}} >
+              <h1>Error : Invalid City Name</h1>
+              <button onClick={handleOff}>X</button>
+            </div>
+
             <MapContainer center={[location.lat, location.lon]} zoom={13} >
-              <TileLayer 
+              <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                 
-                 />
-                <MyMapComponent />
-                <Marker position={[location.lat,location.lon]} icon={customIcon} ></Marker>
+
+              />
+              <MyMapComponent />
+              <Marker position={[location.lat, location.lon]} icon={customIcon} ></Marker>
 
             </MapContainer>
           </div>
